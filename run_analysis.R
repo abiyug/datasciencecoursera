443ab8160cba95c56f8c06d96e1fb03f68e5a3f0
abiyug <abiyug@users.noreply.github.com>
2015-07-30 10:56:29 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -8,6 +8,7 @@
 #                                                                                                                  #
 ####################################################################################################################
 
+# Create a 'data' directory, if it doesn't exist already, download, unzip and save the files from the given URL.
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl, destfile = "./data/SamsungGalaxy.zip", method = "curl")
